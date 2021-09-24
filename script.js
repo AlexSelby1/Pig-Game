c0acb3f7b1b2a93b33fba36ab92471fdff61e6ea
Alex Selby <alex.selby@hotmail.com>
2021-09-24 08:56:46 +0100

    Adding JS and setting dice score to 0

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('Hello');
+console.log();
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.getElementById('dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
