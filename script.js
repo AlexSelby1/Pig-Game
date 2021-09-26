61685e28bc2b9686824e4c240fb6d05ead6900d6
Alex Selby <alex.selby@hotmail.com>
2021-09-26 09:46:58 +0100

    adding switch player function

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-'use strict';
+"use strict";
 
-console.log('Hello');
-console.log();
+console.log("Hello");
 
 // Selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const diceEl = document.getElementById('dice');
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+diceEl.classList.add("hidden");
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice
+btnRoll.addEventListener("click", function () {
+  //Need to generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `images/dice-${dice}.png`;
+
+  //Check for rolled 1, if true, switch to next player
+  if (dice != 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+  }
+});
